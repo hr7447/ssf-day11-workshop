c9190a7abb9f930b5376bf06670895ad6371b7c4
hr7447 <tanhongrui244@gmail.com>
2024-11-11 21:13:12 +0800

    initial commit

---
+++ src/main/java/vttp/batchb/ssf/day11workshop/controllers/Constants.java
@@ -0,0 +1,10 @@
+package vttp.batchb.ssf.day11workshop.controllers;
+
+public class Constants {
+
+    public static final String[] PICTURES = {
+
+        "polarbear.jpg", "cat.jpg"
+    };
+    
+}
---
+++ src/main/java/vttp/batchb/ssf/day11workshop/controllers/PictureOfTheDayController.java
@@ -0,0 +1,29 @@
+package vttp.batchb.ssf.day11workshop.controllers;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//Get / or GET /index.html
+@RequestMapping
+public class PictureOfTheDayController {
+    
+    @GetMapping(path={"/", "/index.html"})
+    public String getIndex(Model model) {
+        model.addAttribute("imageOfTheDay", getPictureOfTheDay());
+        return "picture_of_the_day";
+    }
+
+    private String getPictureOfTheDay() {
+
+        Random rnd = new SecureRandom();
+        int idx = rnd.nextInt(Constants.PICTURES.length);
+        return "/images/%s".formatted(Constants.PICTURES[idx]);
+    }
+
+}
